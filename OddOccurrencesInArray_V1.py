b6529e8031fd568648e98e763b4198436b7ad4fb
Lucio Gutierrez <lucio.alberto@gmail.com>
2019-01-04 11:33:44 -0700

    Update OddOccurrencesInArray_V1.py

--- OddOccurrencesInArray_V1.py
+++ OddOccurrencesInArray_V1.py
@@ -1,5 +1,8 @@
 #OddOccurrencesInArray
 
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
 def solution(A):
     # write your code in Python 3.6
     import re
