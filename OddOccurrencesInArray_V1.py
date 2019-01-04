de6c2972b1a3a98811d01fca22ee66883bda1460
Lucio Gutierrez <lucio.alberto@gmail.com>
2019-01-04 11:27:52 -0700

    Create OddOccurrencesInArray_V1.py

---
+++ OddOccurrencesInArray_V1.py
@@ -0,0 +1,16 @@
+#OddOccurrencesInArray
+
+def solution(A):
+    # write your code in Python 3.6
+    import re
+    element_array = 0
+    string_array = "".join(str(A))
+    #print(string_array)
+    odd_number = 0
+    
+    for number in sorted(A):
+        ocurrence_array = re.findall(str(number), string_array)
+        #print(str(ocurrence_array))
+        if (len(ocurrence_array) % 2 != 0):
+            odd_number = ocurrence_array.pop()
+            return int(odd_number)
